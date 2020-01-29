ffd2c09bac8f9400e029ecbba1da5f265f1c49a8
Kamen Yovchevski <kyovchevski@gmail.com>
2020-01-29 03:21:22 +0100

    Render resources can now be named
    +[RenderResource] Added SetName function

--- Tangra/Tangra/RenderResource.cpp
+++ Tangra/Tangra/RenderResource.cpp
@@ -2,6 +2,12 @@
 
 #include "DirectXTex.h"
 
+void RenderResource::SetName(std::wstring a_NewName)
+{
+    m_DefaultBuffer->SetName(a_NewName.c_str());
+    m_UploadBuffer->SetName((a_NewName + L" Upload buffer").c_str());
+}
+
 RenderResource::~RenderResource()
 {
     
--- Tangra/Tangra/RenderResource.h
+++ Tangra/Tangra/RenderResource.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <d3d12.h>
 #include <wrl.h>
+#include <string>
 struct ServiceLocator;
 
 class RenderResource
@@ -9,6 +10,8 @@ public:
 
     RenderResource() = default;
     RenderResource(ServiceLocator& a_ServiceLocator) : m_Services(a_ServiceLocator) {}
+
+    void SetName(std::wstring a_NewName);
 
     ~RenderResource();
 protected:
